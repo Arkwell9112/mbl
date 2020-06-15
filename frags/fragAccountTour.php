35afd6708e684f6e9dd378767343f20e7dcce9c2
Edward <edward@edsdev.com>
2020-06-15 15:12:53 +0200

    Delivery OK

---
+++ frags/fragAccountTour.php
@@ -0,0 +1,141 @@
+<?php
+require_once("../classes/WeekDay.php");
+require_once("../classes/VallManager.php");
+
+function pp(PDO $bdd)
+{
+    $request = $bdd->prepare("LOCK TABLES global WRITE");
+    $request->execute();
+    $request = $bdd->prepare("SELECT value FROM global WHERE label='currentcustomer'");
+    $request->execute();
+    $result = $request->fetchAll();
+    $currentcustomer = $result[0]["value"];
+    $currentcustomer++;
+    $request = $bdd->prepare("UPDATE global SET value=:value WHERE label='currentcustomer'");
+    $request->execute(array(
+        "value" => $currentcustomer
+    ));
+    $request = $bdd->prepare("UNLOCK TABLES");
+    $request->execute();
+}
+
+if (!isset($_POST["action"])) {
+    $action = "";
+} else {
+    $action = $_POST["action"];
+}
+
+try {
+    $request = $bdd->prepare("SELECT value FROM global WHERE label=:label");
+    $request->execute(array(
+        "label" => "currentcustomer"
+    ));
+    $result = $request->fetchAll();
+    $currentuser = $result[0]["value"];
+    $request = $bdd->prepare("SELECT value FROM global WHERE label=:label");
+    $request->execute(array(
+        "label" => "maxcustomer"
+    ));
+    $result = $request->fetchAll();
+    $maxcustomer = $result[0]["value"];
+    $request = $bdd->prepare("SELECT value FROM global WHERE label=:label");
+    $request->execute(array(
+        "label" => "inittour"
+    ));
+    $result = $request->fetchAll();
+    $inittour = json_decode($result[0]["value"], true);
+    $request = $bdd->prepare("SELECT value FROM global WHERE label=:label");
+    $request->execute(array(
+        "label" => "optitour"
+    ));
+    $result = $request->fetchAll();
+    $optitour = json_decode($result[0]["value"], true);
+} catch (Exception $e) {
+    header("Refresh:0");
+}
+
+if (preg_match("#validate#", $action)) {
+    $customer = $inittour[$optitour[$currentuser]];
+    $command = json_decode($customer["command"], true);
+    $products = array(
+            "Montant" => -$customer[WeekDay::getDay() . "value"]
+    );
+    foreach ($command as $key => $product) {
+        $products[$key] = $product[WeekDay::getDay()];
+    }
+    $content = array(
+        "title" => "paiement commande",
+        "content" => $products
+    );
+    try {
+        $request = $bdd->prepare("SELECT delivered FROM users WHERE username=:username");
+        $request->execute(array(
+            "username" => $customer["username"]
+        ));
+        $result = $request->fetchAll();
+        if ($result[0]["delivered"] == 0) {
+            VallManager::editValue($bdd, -$customer[WeekDay::getDay() . "value"], $customer["username"], $content, "", true);
+        }
+        $request->execute();
+        pp($bdd);
+        header("location: https://monboulangerlivreur.fr/pages/adminaccount.php?page=tour");
+    } catch (Exception $e) {
+        header("Refresh:0");
+    }
+} else if (preg_match("#bypass#", $action)) {
+    try {
+        $request = $bdd->prepare("UPDATE global SET value=value+1 WHERE label='currentcustomer'");
+        $request->execute();
+        pp($bdd);
+        header("location: https://monboulangerlivreur.fr/pages/adminaccount.php?page=tour");
+    } catch (Exception $e) {
+        header("Refresh:0");
+    }
+}
+
+if ($currentuser <= $maxcustomer) {
+    $customer = $inittour[$optitour[$currentuser]];
+    $name = $customer["username"];
+    $address = $customer["address"] . ", " . $customer["city"];
+    $customer = $inittour[$optitour[$currentuser]];
+    $command = json_decode($customer["command"], true);
+} else {
+    $name = "N/A";
+    $address = "La tournée est terminée";
+    $command = array();
+}
+?>
+
+<article id="oncearticle">
+    <div class="title">
+        <img src="../imgs/delivery.svg">
+        <h3>Ma tournée</h3>
+    </div>
+    <div class="signform" id="toreplaceform">
+        <span class="tounderline">Nom d'utilisateur :</span><?php echo " " . $name ?><br><br>
+        <span class="tounderline">Adresse :</span><?php echo " " . $address ?><br><br>
+        <span class="tounderline">Commande :</span><br><br>
+        <?php
+        $products = array();
+        if (!isset($command)) {
+            $command = array();
+        }
+        foreach ($command as $key => $product) {
+            $products[$key] = $product[WeekDay::getDay()];
+        }
+        foreach ($products as $key => $product) {
+            echo $key . " : " . $product . "<br>";
+        }
+        if ($name != "N/A") {
+            echo "<form method=\"post\" action=\"adminaccount.php?page=tour\">
+            <input type=\"hidden\" name=\"action\" value=\"validate\">
+            <input type=\"submit\" value=\"Valider la livraison\">
+        </form>
+        <form method=\"post\" action=\"adminaccount.php?page=tour\">
+            <input type=\"hidden\" name=\"action\" value=\"bypass\">
+            <input type=\"submit\" value=\"Passer la livraison\">
+        </form>";
+        }
+        ?>
+    </div>
+</article>
